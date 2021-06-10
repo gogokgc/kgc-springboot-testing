19062a0decf34aaabad77d7b3cfabb79fd5e52df
gogokgc <gogokgc@gmail.com>
2021-06-10 14:35:48 +0900

    first commit

---
+++ src/main/java/com/kgc/board/KgcSpringbootWebprojectViewtestApplication.java
@@ -0,0 +1,13 @@
+package com.kgc.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KgcSpringbootWebprojectViewtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KgcSpringbootWebprojectViewtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kgc/board/KgcSpringbootWebprojectViewtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgc.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KgcSpringbootWebprojectViewtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
